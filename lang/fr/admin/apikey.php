41a3c86d9c8e77b17be2c4e2efbbfadedfa55b88
SaurFort <contact@saurfort.fr>
2025-04-14 09:57:48 +0200

    Début de la traduction FR

---
+++ lang/fr/admin/apikey.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'title' => 'Clés d\'API d\'application',
+    'empty_table' => 'Aucune clés d\'API.',
+    'whitelist' => 'Adresses IPv4 whitelisté',
+    'whitelist_help' => 'Les clés d\'API peuvent être restreinte pour fonctionner uniquement depuis une IPv4 specifique. Entrer chaque adresses sur une nouvelle ligne.',
+    'whitelist_placeholder' => 'Exemple: 127.0.0.1 ou 192.168.1.1',
+    'description' => 'Description',
+    'description_help' => 'Une description briève, pour quoi cette clé est faite ?',
+    'nav_title' => 'Clés d\'API',
+    'model_label' => 'Clés API de l\'application',
+    'model_label_plural' => 'Clés API de l\'application',
+    'table' => [
+        'key' => 'Clé',
+        'description' => 'Description',
+        'last_used' => 'Dernière utilisation',
+        'created' => 'Créer',
+        'created_by' => 'Créer par',
+        'never_used' => 'Jamais utilisé',
+    ],
+    'permissions' => [
+        'none' => 'Non',
+        'read' => 'Lire',
+        'read_write' => 'Lire & Écrire',
+    ],
+];
---
+++ lang/fr/admin/dashboard.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+    'title' => 'Tableau de bord',
+    'overview' => 'Vue d\'ensemble',
+    'heading' => 'Bienvenu sur Pelican !',
+    'expand_sections' => 'Vous pouvez étendre les sections suivantes :',
+    'version' => 'Version : :version',
+    'advanced' => 'Avancé',
+    'server' => 'Serveur',
+    'user' => 'Utilisateurs',
+    'sections' => [
+        'intro-developers' => [
+            'heading' => 'Information pour les développeurs',
+            'content' => 'Merci d\'essayer la version de développement !',
+            'extra_note' => 'Si vous avez des issues, merci de les reporter sur GitHub.',
+            'button_issues' => 'Créer une issue',
+            'button_features' => 'Discutez des fonctionnalités',
+        ],
+        'intro-update-available' => [
+            'heading' => 'Mise à jour disponible',
+            'content' => 'La :latestVersion est maintenant disponible ! Lisez notre documentation pour mettre à jour votre Panel.',
+        ],
+        'intro-no-update' => [
+            'heading' => 'Votre Panel est à jour',
+            'content' => 'Vous utilisez actuellement la :version. Votre panel est à jour !',
+        ],
+        'intro-first-node' => [
+            'heading' => 'Aucune Nodes détectés',
+            'content' => "Il semble que vous n'avez aucune Nodes configuré actuellement, mais ne vous inquiétez pas parce que vous pouvez aappuyez sur le bouton d'action pour créer votre première !",
+            'extra_note' => 'Si vous avez des issues, merci de le reporter sur GitHub.',
+            'button_label' => 'Créer votre première Node dans Pelican',
+        ],
+        'intro-support' => [
+            'heading' => 'Supporter Pelican',
+            'content' => 'Merci d\'utiliser Pelican, cela est possible uniquement grâce aux soutiens de nos donateurs comme vous, de nos contributeurs, et du reste de nos supportères !',
+            'extra_note' => 'Nous apprécions l\'aide de tout le monde.',
+            'button_translate' => 'Aidez à traduire',
+            'button_donate' => 'Donnez directement',
+        ],
+        'intro-help' => [
+            'heading' => 'Besoin d\'aide ?',
+            'content' => 'Jetez un oeil à notre document d\'abord ! Si vous avez toujours besoin d\'aide passez sur notre serveur discord !',
+            'button_docs' => 'Lire la documentation',
+        ],
+    ],
+];
